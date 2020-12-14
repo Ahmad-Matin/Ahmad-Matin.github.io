1d67cf99663dc510f5a5fb6ca8e1b7234c9e0eb9
Cara Chiang <carachiangeh@gmail.com>
2020-12-14 15:03:32 +0800

    added filter for JSON

--- assets/product.js
+++ assets/product.js
@@ -1,5 +1,7 @@
-const itemsContainer = document.getElementById("burgers");
-function addItem(item) {
+const burgerContainer = document.getElementById("burgers");
+const sidesContainer = document.getElementById("sides");
+
+function addBurger(item) {
     const itemHTML =
         `<div class="col my-2">
 <div class="card border-0 align-items-center">
@@ -15,15 +17,42 @@ function addItem(item) {
 </div>
 `;
 
-    itemsContainer.innerHTML += itemHTML;
+burgerContainer.innerHTML += itemHTML;
+
+
 }
-function fetchProductList() {
+
+function addSides(item) {
+    const itemHTML =
+        `<div class="col my-2">
+<div class="card border-0 align-items-center">
+<div class="card-body">
+<img src =${item.image} class="card-img-top">
+        <h5 class="card-title">${item.name}</h5>
+<p class="card-text">${item.type}</p>
+<p class="card-text">${item.description}</p>
+<p class="card-text">${item.price}</p>
+</div>
+</div>
+</div>
+</div>
+`;
+
+sidesContainer.innerHTML += itemHTML;
+}
+
+function fetchProductList(menuItem) {
     fetch("../assets/product.json")
         .then((response) => response.json())
         .then(response => {
-            itemsContainer.innerHTML = '';
+            burgerContainer.innerHTML = '';
+            sidesContainer.innerHTML = '';
             for (let i = 0; i < response.length; i++) {
-                addItem(response[i]);
+                if (response[i].type == "Burger") {
+                    addBurger(response[i]);
+                } else if (response[i].type == "sides"){
+                    addSides(response[i]);
+                }            
             }
             const productsJson = JSON.stringify(response);
             localStorage.setItem('products', productsJson);
