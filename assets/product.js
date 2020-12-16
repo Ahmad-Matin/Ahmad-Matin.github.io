d4a6fae51329aaeacc95ccf3532ca96a41ef7b19
Cara Chiang <carachiangeh@gmail.com>
2020-12-16 11:03:30 +0800

    added desserts and checkout

--- assets/product.js
+++ assets/product.js
@@ -1,12 +1,13 @@
 const burgerContainer = document.getElementById("burgers");
 const sidesContainer = document.getElementById("sides");
+const dessertsContainer = document.getElementById("desserts");
 
 
 function addBurger(item) {
 
     const itemHTML =
     `<div class="col my-2">
-    <div class="card border-0 align-items-center">
+    <div class="card align-items-center border-0">
     <div class="card-body">
     <img src =${item.img} class="card-img-top rounded menu-img">
     <div class="card-body">
@@ -31,14 +32,14 @@ function addBurger(item) {
 function addSides(item) {
     const itemHTML =
     `<div class="col my-2">
-    <div class="card border-0 align-items-center">
+    <div class="card align-items-center border-0">
     <div class="card-body">
     <img src =${item.img} class="card-img-top rounded menu-img">
     <div class="card-body">
     <h5 class="card-title">${item.name}</h5> 
     ${item.price.map(function(food) {
         return `
-        <li class="list-group-item d-flex justify-content-between">
+        <li class="list-group-item d-flex justify-content-around">
         <div>${food[0]}</div> 
         <div>$${food[1]}</div> 
         <div><a class="btn btn-primary">Add to Cart</a></div>
@@ -52,20 +53,47 @@ function addSides(item) {
     sidesContainer.innerHTML += itemHTML;
 }
 
+function addDesserts(item) {
+    const itemHTML =
+    `<div class="col my-2">
+    <div class="card align-items-center border-0">
+    <div class="card-body">
+    <img src =${item.img} class="card-img-top rounded menu-img">
+    <div class="card-body">
+    <h5 class="card-title">${item.name}</h5> 
+    ${item.price.map(function(food) {
+        return `
+        <li class="list-group-item d-flex justify-content-between">
+        <div>$${food[1]}</div> 
+        <div><a class="btn btn-primary">Add to Cart</a></div>
+        </li>`}).join(" ")} 
+    </div>        
+    </div>
+    </div>
+    </div>
+    `;
 
-function fetchProductList(menuItem) {
+    dessertsContainer.innerHTML += itemHTML;
+}
+
+
+function fetchProductList() {
     fetch("../assets/productsA.json")
         .then((response) => response.json())
         .then(response => {
-            burgerContainer.innerHTML = '';
-            sidesContainer.innerHTML = '';
             for (let i = 0; i < response.length; i++) {
-                if (response[i].type == "burger") {
-                    addBurger(response[i]);
-                } else if (response[i].type == "sides") {
-                    addSides(response[i]);
+                switch (response[i].type) {
+                    case ("burger"):
+                        addBurger(response[i]);
+                        break;
+                    case ("sides"):
+                        addSides(response[i]);
+                        break;
+                    case ("dessert"):
+                        addDesserts(response[i]);
+                        break;
+                    } 
                 }
-            }
             const productsJson = JSON.stringify(response);
             localStorage.setItem('products', productsJson);
         })
