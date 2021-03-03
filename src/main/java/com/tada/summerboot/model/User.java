4bc9edc06140402f898a7e56d7de80e5d8e9c93b
Ahmad-Matin <ahmadmatin.fa@gmai.com>
2021-03-04 03:51:16 +0800

    update

--- src/main/java/com/tada/summerboot/model/User.java
+++ src/main/java/com/tada/summerboot/model/User.java
@@ -1,13 +1,13 @@
 package com.tada.summerboot.model;
 
-//import lombok.Getter;
-//import lombok.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 @Entity
 public class User {
 
