aa6782502ac523976fb2914dd0b7fc669263e071
Cara Chiang <carachiangeh@gmail.com>
2021-03-03 18:15:58 +0800

    updated css on admin page, added cancel buttons

--- target/classes/static/assets/admin2.js
+++ target/classes/static/assets/admin2.js
@@ -1,4 +1,9 @@
 document.getElementById("add-new-product").addEventListener("click", () => {
-const form = document.getElementById("add-product-form");
+let form = document.getElementById("add-product-form");
 form.classList.remove("hide");
+})
+
+document.getElementById("cancel-add-item").addEventListener("click", () => {
+let form = document.getElementById("add-product-form");
+form.classList.add("hide");
 })
